22b5e43935ea2e139b15dd1b7522327f9b94d4a7
CjSystem2030 <cristian_56_90@hotmail.com>
2022-04-20 18:16:20 -0500

    update

---
+++ src/hooks/useCotizador.jsx
@@ -0,0 +1,9 @@
+import { useContext } from 'react'
+import CotizadorContext from '../context/CotizadorProvider'
+
+const useCotizador = () => {
+    return useContext(CotizadorContext)
+}
+
+export default useCotizador
+
